d3c563ccacf7377b8df245e662239ac2ce80ef76
wei06097 <wei06097@gmail.com>
2023-02-17 06:08:04 -0800

    Add api url

---
+++ src/global/API.js
@@ -0,0 +1,12 @@
+const API_URL = process.env.REACT_APP_API_URL
+
+const SIGNUP = `${API_URL}/signup`
+const LOGIN = `${API_URL}/login`
+const JWT = `${API_URL}/jwt`
+
+const API = {
+    SIGNUP,
+    LOGIN,
+    JWT
+}
+export default API
