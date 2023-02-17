69c540c3e742962a84fd6103f3fff72413ddf3cb
wei06097 <wei06097@gmail.com>
2023-02-17 08:04:36 -0800

    --

--- src/global/API.js
+++ src/global/API.js
@@ -1,10 +1,10 @@
 const API_URL = process.env.REACT_APP_API_URL
-
 const SIGNUP = `${API_URL}/signup`
 const LOGIN = `${API_URL}/login`
 const JWT = `${API_URL}/jwt`
 
 const API = {
+    API_URL,
     SIGNUP,
     LOGIN,
     JWT
