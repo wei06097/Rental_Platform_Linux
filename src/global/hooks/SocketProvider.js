7854fd42759d39ea8bffb18a7c312ba9cb068227
wei06097 <wei06097@gmail.com>
2023-02-26 09:01:04 -0800

    Add automatic token detector

--- src/global/hooks/SocketProvider.js
+++ src/global/hooks/SocketProvider.js
@@ -8,20 +8,18 @@ const SocketContext = React.createContext()
 export const useSocket = () => useContext(SocketContext)
 
 export default function SocketProvider({ children }) {
+    // 自動偵測localStorage的token，如果沒有token就重新載入(socket會重新連線)
+    window.addEventListener("storage", (event) => {
+        const isLogout = event.key === "token" && !event.newValue
+        if (isLogout) window.location.reload()
+    })
+    const token = localStorage.getItem("token")
+    const account = localStorage.getItem("account")
+    if (token && account) socket.emit("login", token, account)
+
     return <>
         <SocketContext.Provider value={socket}>
             {children}
         </SocketContext.Provider>
     </>
 }
-
-/* 前端要讓後端知道的動作
- *
- * 進來網頁: 加入 socket.io 但不要進房間 (房間最多2人 => 私訊)
- * 進來聊天: 還是不要進房間
- * 進入對話 (選擇特定人): 進房間
- * */
-
-/* DB 何時加入聊天歷史紀錄
- * 房間內有人傳訊息: 將對話加到 DB，並且 broadcast 給房間的所有人 (這邊指另 1 個人)
- * */
