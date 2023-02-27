e29ce90dc05097838dcd03baf217277dca141662
wei06097 <wei06097@gmail.com>
2023-02-27 07:50:37 -0800

    --

--- src/global/API.js
+++ src/global/API.js
@@ -4,11 +4,13 @@ const WS_URL = API_URL
 const SIGNUP = `${API_URL}/signup`
 const LOGIN = `${API_URL}/login`
 const JWT = `${API_URL}/jwt`
+const CHATROOM = `${API_URL}/chatroom`
 
 const API = {
     WS_URL,
     SIGNUP,
     LOGIN,
-    JWT
+    JWT,
+    CHATROOM
 }
 export default API
