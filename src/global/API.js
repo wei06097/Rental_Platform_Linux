ff908d711e09280a5d34ee6a65eebec557c85951
Pain <wei06097@gmail.com>
2023-03-04 14:24:11 +0800

    --

--- src/global/API.js
+++ src/global/API.js
@@ -4,11 +4,17 @@ const WS_URL = API_URL
 const SIGNUP = `${API_URL}/signup`
 const LOGIN = `${API_URL}/login`
 const JWT = `${API_URL}/jwt`
-const CHATLIST = `${API_URL}/chatlist`
-const CHATROOM = `${API_URL}/chatroom`
+
+const CHAT_LIST = `${API_URL}/chat_list`
+const CHAT_HISTORY = `${API_URL}/chat_history`
+
 const ADD_PRODUCT = `${API_URL}/add_product`
 const EDIT_PRODUCT = `${API_URL}/edit_product`
 const SAVE_PRODUCT = `${API_URL}/save_product`
+
+const MY_PRODUCTS = `${API_URL}/my_products`
+const DELETE_PRODUCT =  `${API_URL}/delete_product`
+const LAUNCH_PRODUCT =  `${API_URL}/launch_product`
 
 export async function get(url) {
     const response = await fetch(url)
@@ -31,7 +37,8 @@ const API = {
     // constant
     WS_URL,
     SIGNUP, LOGIN, JWT,
-    CHATLIST, CHATROOM,
-    ADD_PRODUCT, EDIT_PRODUCT, SAVE_PRODUCT
+    CHAT_LIST, CHAT_HISTORY,
+    ADD_PRODUCT, EDIT_PRODUCT, SAVE_PRODUCT,
+    MY_PRODUCTS, DELETE_PRODUCT, LAUNCH_PRODUCT
 }
 export default API
