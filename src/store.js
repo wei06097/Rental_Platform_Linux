9f1e19aed4e751b6c7943a2de01cbd70a2cb8880
wei06097 <wei06097>
2023-06-23 13:00:55 -0700

    使用 redux-persist

--- src/store.js
+++ src/store.js
@@ -1,10 +1,41 @@
+import thunk from "redux-thunk"
+import { combineReducers } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
-import myProductSlice from "./store/myProductSlice"
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import storageSession from "redux-persist/lib/storage/session"
 
+import accountSlice from "./store/accountSlice"
+import homepageSlice from "./store/homepageSlice"
+import myProductSlice from "./store/myProductSlice"
+import editProductSlice from "./store/editProductSlice.js"
+
+/* ======================================== */
+const account_PersistConfig = {
+    key : "account",
+    storage : storage
+}
+const homepage_PersistConfig = {
+    key : "homepage",
+    storage : storageSession
+}
+const myProduct_PersistConfig = {
+    key : "myProduct",
+    storage : storageSession
+}
+
+/* ======================================== */
+const reducer = combineReducers({
+    account : persistReducer(account_PersistConfig, accountSlice),
+    homepage : persistReducer(homepage_PersistConfig, homepageSlice),
+    myProduct : persistReducer(myProduct_PersistConfig, myProductSlice),
+    editProduct : editProductSlice
+})
 const store = configureStore({
-    reducer : {
-        myProduct : myProductSlice
-    }
+    reducer : reducer,
+    middleware : [thunk]
 })
 
+/* ======================================== */
+export const persistor = persistStore(store)
 export default store
