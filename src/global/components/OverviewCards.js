d82e2b57cf2ce73fa1c4f8abc1b2751c90d77a43
wei06097 <wei06097>
2023-06-25 06:05:27 -0700

    修改 樣式

--- src/global/components/OverviewCards.js
+++ src/global/components/OverviewCards.js
@@ -1,20 +1,39 @@
+/* import */
 /* CSS */
 import style from "./OverviewCards.module.css"
-
-/* React Hooks */
+/* Hooks */
+import { useState, useEffect, useRef } from "react"
 import { Link } from "react-router-dom"
 
-function ProductCard({id, link, name, price, showHeart }) {
+/* ======================================== */
+function ProductCard({ id, link, name, price, showHeart }) {
+    const [loaded, setLoaded] = useState(false)
+    const imgRef = useRef()
+    useEffect(() => {
+        const imgElement = imgRef.current
+        const loaded = () => setLoaded(true)
+        imgElement.addEventListener("load", loaded)
+        return () => {
+            imgElement.removeEventListener("load", loaded)
+        }
+    }, [])
+
+    /* ==================== 分隔線 ==================== */
     return <>
         <div className={style.product_card} >
-            <Link to={`/Product/${id}`}>
+            <Link className="link" to={`/Product/${id}`}>
                 <div className={style.img}>
-                    <img src={link} alt="" />
+                    {!loaded && <div className="fill skeleton" />}
+                    <img style={{opacity:loaded?1:0}}
+                        src={link} alt="" ref={imgRef}
+                    />
                 </div>
-                <div className={style.name}>{name}</div>
-                <div className={style.price}>NT$ {price} / 每天</div>
+                <div className={style.info}>
+                    <div>{name}</div>
+                    <div>{`NT$${price} / 每天`}</div>
+                </div>
             </Link>
-            <button className={`${style.heart} ${showHeart? "": style.none}`}>
+            <button className={`${style.heart}`} style={{display:showHeart?"block":"none"}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                 </svg>
@@ -22,11 +41,12 @@ function ProductCard({id, link, name, price, showHeart }) {
         </div>
     </>
 }
-
 function OverviewCards({ children }) {
-    return <div className={style.products_area}>
-        {children}
-    </div>
+    return <>
+        <div className={style.products_area}>
+            {children}
+        </div>
+    </>
 }
 
 OverviewCards.ProductCard = ProductCard
