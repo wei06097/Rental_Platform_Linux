2ea200d3ae8a1d1b417f56164e8ca136df66c837
wei06097 <wei06097>
2023-06-26 00:24:09 -0700

    更改排版

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,22 @@
 /* Components: icon */
 import Back from "../global/icon/Back"
+import Home from "../global/icon/Home"
 
 export default function NotFound() {
     return <>
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-            <Back />
-            <h1 style={{textAlign: "center"}}>404 Not Found</h1>
-        </div>
+        <header>
+            <div className="flex_center">
+                <Back />
+                <div>Go Back</div>
+            </div>
+            <div className="flex_center">
+                <Home />
+            </div>
+        </header>
+        <main style={{height: "calc(100vh - var(--height))"}}>
+            <div style={{height:"50%", display:"grid", placeItems:"center"}}>
+                <h1 style={{fontSize: "50px"}}>404 Not Found</h1>
+            </div>
+        </main>
     </>
 }
