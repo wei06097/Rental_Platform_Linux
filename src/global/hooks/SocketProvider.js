26d18b1e1fecf76c89c8b5e901ee376a8324a600
wei06097 <wei06097>
2023-06-27 09:46:05 -0700

    改成能依據登入狀態自動連接後端

--- src/global/hooks/SocketProvider.js
+++ src/global/hooks/SocketProvider.js
@@ -1,25 +1,51 @@
+/* import */
+/* ======================================== */
+/* API */
 import API from "../../API"
-
-import React, { useContext } from "react"
+/* socket */
+import React, { useState, useEffect, useContext } from "react"
 import webSocket from "socket.io-client"
+/* Redux */
+import { useSelector } from "react-redux"
 
-const socket = webSocket(API.WS_URL)
 const SocketContext = React.createContext()
 export const useSocket = () => useContext(SocketContext)
-
+/* ======================================== */
 export default function SocketProvider({ children }) {
-    // 自動偵測localStorage
-    window.addEventListener("storage", (event) => {
-        const isLogout = event.key === "token" && !event.newValue
-        // 為了讓socket重新連線，必須重新整理
-        if (isLogout) window.location.reload()
-    })
-    socket.on('connect', () => {
-        const token = localStorage.getItem("token")
-        const account = localStorage.getItem("account")
-        if (!token && !account) return
-        socket.emit("login", token, account)
-    })
+    const [socket, setSocket] = useState(null)
+    const {token} = useSelector(state => state.account)
+
+    useEffect(() => {
+        if (!token) return
+        const newSocket = webSocket(API.WS_URL, {
+            extraHeaders : {
+                "Authorization": token
+            }
+        })
+        setSocket(newSocket)
+        return () => {
+            newSocket.close()
+        }
+    }, [token])
+    
+    useEffect(() => {
+        // 登出了但還沒斷線
+        if (!token && socket) setSocket(null)
+    }, [token, socket])
+
+    useEffect(() => {
+        if (!socket) return
+        socket.on("connect", () => {
+            console.log("socket 已連線", socket)
+            // console.log("token:", socket._opts.extraHeaders.Authorization)
+        })
+        return () => {
+            socket.off("connect")
+            console.log("socket 已斷線")
+        }
+    }, [socket])
+
+    /* ==================== 分隔線 ==================== */
     return <>
         <SocketContext.Provider value={socket}>
             {children}
