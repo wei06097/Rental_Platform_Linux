470d98e044f0d9f81e76d894e25a644f181faaaa
wei06097 <wei06097>
2023-07-02 11:13:21 -0700

    增加直接租借功能

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -54,7 +54,16 @@ export default function Product() {
         if (success) setIsAdded(prev => !prev)
         setIsHandling(false)
     }
-    
+    async function rentImmediately() {
+        if (!account) navigate("/SignIn")
+        setIsHandling(true)
+        const {success} = isAdded
+            ? {success : true}
+            : await API.post(`${API.CRUD_CART}/?id=${id}`, token)
+        if (success) navigate(`/Bill/${product?.provider}`)
+        setIsHandling(false)
+    }
+
     /* ==================== 分隔線 ==================== */
     return <>
         {
@@ -146,7 +155,8 @@ export default function Product() {
                             </>
                         }         
                     </button>
-                    <button className="button buttonText grow">
+                    <button className="button buttonText grow"
+                        onClick={rentImmediately} disabled={isLoading || isHandling}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="icon bi bi-cart-fill" viewBox="0 0 16 16">
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                         </svg>
